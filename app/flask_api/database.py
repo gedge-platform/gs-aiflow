555948cfcc71ac342883d31416094f570752512b
Dongle94 <sdy1029@gmail.com>
2023-03-31 15:07:33 +0900

    backend server init_db - database

--- app/flask_api/database.py
+++ app/flask_api/database.py
@@ -9,27 +9,36 @@ dbUser = os.getenv('DB_USER', 'admin')
 dbPass = os.getenv('DB_PASS', 'admin')
 
 def create_tables(dbcon=None):
-    mycon = mysql.connector.connect(
-        host=dbHost,
-        port=dbPort,
-        user=dbUser,
-        password=dbPass,
-    )
+    if dbcon is None:
+        dbcon = mysql.connector.connect(
+            host=dbHost,
+            port=dbPort,
+            user=dbUser,
+            password=dbPass,
+        )
 
-    cursor = mycon.cursor()
+    cursor = dbcon.cursor()
+    cursor.execute(
+        'SHOW DATABASES'
+    )
+    rets = cursor.fetchall()
+
+    if ('aiflow',) in rets:
+        return print("DATABASE ALREADY EXIST")
+
     cursor.execute('SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED')
-    cursor.execute(f'CREATE DATABASE IF NOT EXISTS {"aieyeflow"} CHARACTER SET utf8')
-    cursor.execute('USE aieyeflow')
-    mycon.commit()
+    cursor.execute(f'CREATE DATABASE IF NOT EXISTS {"aiflow"} CHARACTER SET utf8')
+    cursor.execute('USE aiflow')
+    dbcon.commit()
 
     import codecs
     with codecs.open(filename='./runtime_data/clusterDB.sql', mode='r', encoding='utf-8') as f:
         sqls = f.read()
-    cursor = mycon.cursor()
+    cursor = dbcon.cursor()
     rs = cursor.execute(sqls, multi=True)
     for r in rs:
         pass
-    mycon.commit()
+    dbcon.commit()
 
     return print("SUCCESS CREATE DATABASE")
 
@@ -37,10 +46,10 @@ def create_tables(dbcon=None):
 def get_db_connection():
     if not g_var.mycon:
         g_var.mycon = mysql.connector.connect(
-        host=dbHost,
-        port=dbPort,
-        user=dbUser,
-        database='aieyeflow',
-        password=dbPass,
+            host=dbHost,
+            port=dbPort,
+            user=dbUser,
+            database='aiflow',
+            password=dbPass,
         )
     return g_var.mycon
