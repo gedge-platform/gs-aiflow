500fc8416cf1e3b2fedf17b99562586036a70446
JinRyua <aopad240@gmail.com>
2023-05-11 14:20:04 +0900

    -  각 task가 자신의 이름으로 log file 남기도록 yaml 수정

--- app/flask_api/runtime_helper.py
+++ app/flask_api/runtime_helper.py
@@ -7,7 +7,7 @@ def getBasicYaml(userID, projectName, projectID, nodeID):
             'spec': {'restartPolicy': 'Never', 'nodeName': 'gedgeworker1', 'containers': [
                 {'name': 'ubuntu', 'image': 'yolov5:v0.0.230503', 'imagePullPolicy': 'IfNotPresent',
                  'command': ['/bin/bash', '-c'], 'args': [
-                    'source /root/path.sh; PATH=/opt/conda/envs/pt1.12.1_py38/bin:/root/volume/cuda/cuda-11.3/bin:$PATH; env; cd /root/yolov5; python train.py --project /root/user --name yolo_coco128_train --data ~/volume/dataset/coco128/coco128.yaml --device 0 --weights ./weights/yolov5s-v7.0.pt --epochs 1 --batch 1'],
+                    'source /root/path.sh; PATH=/opt/conda/envs/pt1.12.1_py38/bin:/root/volume/cuda/cuda-11.3/bin:$PATH; env; mkdir -p /root/user/logs; cd /root/yolov5; nohup python train.py --project /root/user --name yolo_coco128_train --data ~/volume/dataset/coco128/coco128.yaml --device 0 --weights ./weights/yolov5s-v7.0.pt --epochs 1 --batch 1  &>> /root/user/logs/' + nodeID + '.log'],
                  'env': [{'name': 'LD_LIBRARY_PATH',
                           'value': '/root/volume/cuda/cuda-11.3/lib64:/root/volume/cudnn/cuda-cudnn-8.3/lib64:/root/volume/tensorrt/TensorRT-8.4.3.1-cuda-11/lib'}],
                  'resources': {'limits': {'cpu': '4', 'memory': '8G', 'nvidia.com/gpu': '1'}}, 'volumeMounts': [
@@ -105,18 +105,18 @@ def makeYamlTrainRuntime(userID, projectName, projectID, node_id, runtime, model
 
 def makeYamlValidateRuntime(userID, projectName, projectID, node_id, runtime, model, tensorRT, cuda):
     data = getBasicYaml(userID, projectName, projectID, node_id)
-    data['spec']['containers'][0]['args'] = ['source /root/path.sh; PATH=/opt/conda/envs/pt1.12.1_py38/bin:/root/volume/cuda/cuda-11.3/bin:$PATH; env; cd /root/yolov5; python val.py --project /root/user --name yolo_coco128_validate --data ~/volume/dataset/coco128/coco128.yaml --device 0 --weights /root/user/yolo_coco128_train/weights/best.pt --batch-size 1']
+    data['spec']['containers'][0]['args'] = ['source /root/path.sh; PATH=/opt/conda/envs/pt1.12.1_py38/bin:/root/volume/cuda/cuda-11.3/bin:$PATH; env; mkdir -p /root/user/logs; cd /root/yolov5; nohup python val.py --project /root/user --name yolo_coco128_validate --data ~/volume/dataset/coco128/coco128.yaml --device 0 --weights /root/user/yolo_coco128_train/weights/best.pt --batch-size 1 &>> /root/user/logs/' + node_id + '.log']
 
     return data
 def makeYamlOptimizationRuntime(userID, projectName, projectID, node_id, runtime, model, tensorRT, cuda):
     data = getBasicYaml(userID, projectName, projectID, node_id)
-    data['spec']['containers'][0]['args'] = ['source /root/path.sh; PATH=/opt/conda/envs/pt1.12.1_py38/bin:/root/volume/cuda/cuda-11.3/bin:$PATH; env; cd /root/yolov5; python export.py --weights /root/user/yolo_coco128_train/weights/best.pt --include engine --device 0 --half --batch-size 1 --imgsz 640']
+    data['spec']['containers'][0]['args'] = ['source /root/path.sh; PATH=/opt/conda/envs/pt1.12.1_py38/bin:/root/volume/cuda/cuda-11.3/bin:$PATH; env; mkdir -p /root/user/logs; cd /root/yolov5; nohup python export.py --weights /root/user/yolo_coco128_train/weights/best.pt --include engine --device 0 --half --batch-size 1 --imgsz 640 &>> /root/user/logs/' + node_id + '.log']
 
     return data
 
 def makeYamlOptValidateRuntime(userID, projectName, projectID, node_id, runtime, model, tensorRT, cuda):
     data = getBasicYaml(userID, projectName, projectID, node_id)
-    data['spec']['containers'][0]['args'] = ['source /root/path.sh; PATH=/opt/conda/envs/pt1.12.1_py38/bin:/root/volume/cuda/cuda-11.3/bin:$PATH; env; cd /root/yolov5; python val.py --project /root/user --name yolo_coco128_opt_validate --weights /root/user/yolo_coco128_train/weights/best.engine --data ~/volume/dataset/coco128/coco128.yaml --device 0 --batch-size 1 --imgsz 640']
+    data['spec']['containers'][0]['args'] = ['source /root/path.sh; PATH=/opt/conda/envs/pt1.12.1_py38/bin:/root/volume/cuda/cuda-11.3/bin:$PATH; env; mkdir -p /root/user/logs; cd /root/yolov5; nohup python val.py --project /root/user --name yolo_coco128_opt_validate --weights /root/user/yolo_coco128_train/weights/best.engine --data ~/volume/dataset/coco128/coco128.yaml --device 0 --batch-size 1 --imgsz 640 &>> /root/user/logs/' + node_id + '.log']
 
     return data
 
